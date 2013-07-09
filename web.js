3621997770325eb5fcd20ca2e39d84e30ce6c45b
Esteban Afonso <esteban.afonso@gmail.com>
2013-07-09 14:43:04 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var homepage = fs.readFileSync(index.html);
+    var homepage = fs.readFileSync("index.html","utf-8");
     response.send(homepage);
 });
 
