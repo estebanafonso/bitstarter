92907f455e4d39ac193b323fd618134b06ed54ba
Esteban Afonso <esteban.afonso@gmail.com>
2013-07-09 14:19:20 +0000

    retry

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html","utf-8"));
+    var homepage = fs.readFileSync(index.html);
+    response.send(homepage);
 });
 
 var port = process.env.PORT || 5000;
